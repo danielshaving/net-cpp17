054458b8416b36db196c1dd353ea1d6d27e7f083
danielshaving <474711079@qq.com>
2018-05-21 17:33:47 +0800

    format

--- src/callback.h
+++ src/callback.h
@@ -1,15 +1,8 @@
 #pragma once
 #include "all.h"
-class Buffer;
-class RedisAsyncContext;
 class TcpConnection;
 class Connector;
-class RedisContext;
-class RedisReader;
-class HiredisAsync;
 class TcpClient;
-class Session;
-class Item;
 class ThreadPool;
 class Acceptor;
 class Channel;
@@ -17,17 +10,11 @@ class TimerQueue;
 class Poll;
 class Epoll;
 class Thread;
-typedef std::shared_ptr<HiredisAsync> HiredisAsyncPtr;
-typedef std::shared_ptr<Buffer> xBufferPtr;
-typedef std::shared_ptr<RedisReader> RedisReaderPtr;
-typedef std::shared_ptr<RedisContext> RedisContextPtr;
-typedef std::shared_ptr<RedisAsyncContext> RedisAsyncContextPtr;
+class Buffer;
+
 typedef std::shared_ptr<TcpConnection> TcpConnectionPtr;
 typedef std::shared_ptr<Connector> ConnectorPtr;
 typedef std::shared_ptr<TcpClient> TcpClientPtr;
-typedef std::shared_ptr<Session> SessionPtr;
-typedef std::shared_ptr<Item> ItemPtr;
-typedef std::shared_ptr<const Item> ConstItemPtr;
 typedef std::shared_ptr<ThreadPool> ThreadPoolPtr;
 typedef std::unique_ptr<Acceptor> AcceptorPtr;
 typedef std::shared_ptr<Channel> ChannelPtr;
